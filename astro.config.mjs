1870306ed919349e89960bdb70ee39c27c9b55e2
ree <hannn9491@gmail.com>
2024-07-30 13:50:50 +0700

    adding web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 /* 
   We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
@@ -36,4 +37,8 @@ export default defineConfig({
       config: { applyBaseStyles: false },
     }),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {enabled: true}
+  }),
 });
