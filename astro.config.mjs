eb6b660ecdf5cde54f66c4b671db08925eb95190
ree <hannn9491@gmail.com>
2024-08-03 15:40:08 +0700

    delete web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 
 /* 
@@ -38,13 +37,5 @@ export default defineConfig({
       config: { applyBaseStyles: false },
     }),
   ],
-  output: 'server',
-  adapter: vercel(
-    {
-      webAnalytics: {
-        enabled: true,
-      }
-    }
-  ),
 });
 
